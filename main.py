f6842511e47d2e948fd413e171fd369aec6c32ba
Marcel Garcia <garciamsu@gmail.com>
2023-10-06 11:46:00 -0400

    actualizacion del codigo

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from os import remove
 import os
 
 #Initialitation
-password = "password"
-event = "feria-de-autos-peru"
+password = "123456"
+event = "ferialab-2-2023"
+role = "visit"
+rights = "any:any"
+
 
 #Remove result'file
 if os.path.isfile(event +".csv"):
@@ -22,7 +25,7 @@ if df_comma.shape[1] > df_semi.shape[1]:
 else:
     origin = df_semi
 
-df_comma = pd.read_csv(pathname + "/source/mirror.csv", sep=",", encoding='latin1')
+df_comma = pd.read_csv(pathname + "/source/mirror.csv", sep=";", encoding='latin1')
 df_semi = pd.read_csv(pathname + "/source/mirror.csv", sep=";", encoding='latin1')
 if df_comma.shape[1] > df_semi.shape[1]:
     mirror = df_comma
@@ -35,20 +38,18 @@ origin = origin.drop_duplicates(['email'])
 print(origin.size/2)
 
 print("Processing information, please wait... ")
-result = pd.DataFrame(columns=['name', 'email'])
+result = pd.DataFrame(columns=['name', 'email', 'password', 'event', 'role', 'rights'])
 count = 0
 for index1, row1 in origin.iterrows():
     # Get the email
     email1 = row1["email"]
     
-    if (row1["email"] == "mayreebok@hotmail.com"):
-        print(row1["name"] + " " + row1["email"])
-        
     if (not (row1["email"] in mirror.email.values)):
         count = count + 1
-        result.append(row1)
-        result=result.append({'name' : row1["name"] , 'email' : row1["email"], 'password' : password, 'event': event } , ignore_index=True)
-    
+
+        # https://es.stackoverflow.com/questions/525559/futurewarning-the-frame-append-method-is-deprecated-and-will-be-removed-from-pa
+        result = pd.concat([result, pd.DataFrame({'name' : row1["name"] , 'email' : row1["email"], 'password' : password, 'event': event , 'role': role, 'rights': rights }, index=[0])], ignore_index=True)
+
 print(result)
 print(result.size)
 result.to_csv("UsuariosCSV.csv", encoding ='latin1', index=False)
